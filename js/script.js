03c1795266093b0f7d437488afd3f1e5c69fb47f
AkermanX <39915009+AkermanX@users.noreply.github.com>
2018-11-06 01:59:32 -0600

    FirtsPart
    Primera subida

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function tamañoal (x){
+	document.getElementById("aladino").poster="img/aladino1.jpeg";
+
+}
+function reduceal (x){
+	document.getElementById("aladino").poster="img/aladino.jpeg";
+
+}
+function grande (x){
+	document.getElementById("capitana").poster="img/capitana.jpeg";
+
+}
+function pequeño (x){
+	document.getElementById("capitana").poster="img/capitana2.jpeg";
+
+}
